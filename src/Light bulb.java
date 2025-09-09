e46a6603d7c2c7f8f2458410eaaeb3e2a06b8280
Manibharathi <bmani1733@gmail.com>
2025-09-09 14:56:21 +0530

    Light bulb Test

---
+++ src/Light bulb.java
@@ -0,0 +1,15 @@
+public class Hello_World {
+    public static void main(String[] args) {
+        int n = 7; // input
+
+        if (  n % 2 == 0)
+        {
+            System.out.println("ON");
+        }
+
+        else
+        {
+            System.out.println("OFF");
+        }
+    }
+}
